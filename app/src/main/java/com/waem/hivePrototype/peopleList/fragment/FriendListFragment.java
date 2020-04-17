6f8cc28015ff20018b2bcb696bce89eeaee4341d
전지민 <jimin10722@waem.kr>
2020-04-17 16:39:09 +0900

    버터나이프 추가

--- app/src/main/java/com/waem/hivePrototype/peopleList/fragment/FriendListFragment.java
+++ app/src/main/java/com/waem/hivePrototype/peopleList/fragment/FriendListFragment.java
@@ -13,14 +13,19 @@ import com.waem.hivePrototype.peopleList.vo.People;
 import java.util.ArrayList;
 
 public class FriendListFragment extends Fragment {
-	private ArrayList<People> friendA
+	private ArrayList<People> friendArray =new ArrayList<>();
 	private View view;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		view =  inflater.inflate(R.layout.fragment_friend_list, container, false);
 
-
+		init();
 		return view;
+	}
+	private void init(){
+		People people1 =new People();
+
+		friendArray.add(people1);
 	}
 }
